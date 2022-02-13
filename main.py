b5922b1ebb960f41f7071d25e16c9c4178d622fd
Goodoasis <de.thibault@gmail.com>
2022-02-14 00:37:27 +0100

    Externalise Data

--- main.py
+++ main.py
@@ -1,84 +1,24 @@
+import json
 import base64
+import random
 from io import BytesIO
 from sys import maxsize as sys_maxsize
-import random
 
 from PIL import Image, ImageDraw, ImageFilter
 import streamlit as st
 
 
-back_garden = {
-    'light-cyan': "#cffcff",
-    'magic-mint': "#aaefdf",
-    'light-green': "#9ee37d",
-    'kelly-green': "#63c132",
-    'india-green': "#358600ff"
-    }
+@st.cache(show_spinner=False)
+def get_palettesColors():
+    with open(r"data\\colors.json", 'r') as f:
+        data = json.load(f)
+    return data
 
-kill_bill = {
-    'black': "#000000",
-    'oxford-blue': "#14213d",
-    'orange-web': "#fca311",
-    'platinum': "#e5e5e5",
-    'white': "#ffffff"
-    }
-
-colors_summer = {
-    'amethyst': '#9b5de5',
-    'magenta-crayola': '#f15bb5',
-    'minion-yellow': '#fee440',
-    'capri': '#00bbf9',
-    'sea-green-crayola': '#00f5d4'
-    }
-
-colors_coffee = {
-    'almond': '#ede0d4',
-    'desert-sand': '#e6ccb2',
-    'tan': '#ddb892',
-    'cafe-au-lait': '#b08968',
-    'coffee': '#7f5539',
-    'brown-sugar': '#9c6644'
-    }
-
-colors_sky = {
-    'flickr-pink': '#f72585',
-    'byzantine': '#b5179e',
-    'purple': '#7209b7',
-    'purple-2': '#560bad',
-    'trypan-blue': '#480ca8',
-    'trypan-blue-2': '#3a0ca3',
-    'persian-blue': '#3f37c9',
-    'ultramarine-blue': '#4361ee',
-    'dodger-blue': '#4895ef',
-    'vivid-sky-blue': '#4cc9f0'
-    }
-
-cupcake = {
-    'cotton-candy': "#fdc5f5",
-    'mauve': "#f7aef8",
-    'lavender-floral': "#b388eb",
-    'cornflower-blue': "#8093f1",
-    'sky-blue-crayola': "#72ddf7"
-    }
-
-default_colors = {
-    "0": '#7cb8e6',
-    "1": '#a587e8',
-    "3": '#e687ba',
-    "4": '#c0f59d',
-    "5": '#f5ef9d',
-    "6": '#f5a09d',
-    "7": '#b49df5',
-    "8": '#f8f7fa'
-    }
-
-PALETTES_NAMES = ["Back garden", "Kill_Bill", "Summer", "Coffee", "Sky", "Cup Cake", "Pastelle"]
+COLORS_DATA = get_palettesColors()
+PALETTES_NAMES = list(COLORS_DATA.keys())
 
 if "palettes_list" not in st.session_state:
-    st.session_state["palettes_list"] = [back_garden, kill_bill, colors_summer, colors_coffee, colors_sky, cupcake, default_colors]
-
-if "palette_index" not in st.session_state:
-    st.session_state["palette_index"] = None
+    st.session_state["palettes_list"] = list(COLORS_DATA.values())
 
 if "palette" not in st.session_state:
     st.session_state["palette"] = None
@@ -95,17 +35,12 @@ if "args" not in st.session_state:
 
 def change_args(*args):
     seed, palette = args
-    if palette == False:
+    if (palette == False) or (isinstance(palette, str)):
         idx = PALETTES_NAMES.index(radio)
         palette = st.session_state.palettes_list[idx]
-    elif isinstance(palette, str):
-        idx = PALETTES_NAMES.index(radio)
-        palette = st.session_state.palettes_list[idx]
-    elif palette is not None:
-        st.session_state.palette_index = st.session_state.palettes_list.index(palette)
-    args = seed, palette
+    new_args = seed, palette
     st.session_state.palette = palette
-    st.session_state.args = args
+    st.session_state.args = new_args
 
 def random_palette(seed):
     if seed == None:
@@ -120,14 +55,14 @@ def random_color(palette):
 
 # Side bar
 st.sidebar.subheader("Resolution")
-width_box = st.sidebar.number_input("width", min_value=10, max_value=6000, value=1920, step=1, on_change=None)
-height_box = st.sidebar.number_input("height", min_value=10, max_value=5000, value=1080, step=1, on_change=None)
+width_box = st.sidebar.number_input("width", min_value=10, max_value=6000, value=1920, step=1)
+height_box = st.sidebar.number_input("height", min_value=10, max_value=5000, value=1080, step=1)
 
 # Slider for grid dimensions.
 grid_expander = st.sidebar.expander("Grid dimensions", expanded=False)
 with grid_expander:
-    grid_h = st.slider("rows", min_value=1, key='grid_h', max_value=6, value=2, step=1, on_change=None)
-    grid_w = st.slider("columns", min_value=1, key='grid_w', max_value=6, value=3, step=1, on_change=None)
+    grid_h = st.slider("rows", min_value=1, max_value=6, value=2, step=1)
+    grid_w = st.slider("columns", min_value=1, max_value=6, value=3, step=1)
 
 # Slider for blur strengh
 st.sidebar.header("")
@@ -142,15 +77,13 @@ def generate_img(*args):
         palette = random_palette(seed)
     random.seed(seed)
     st.session_state.palette = palette
-    st.session_state.palette_index = st.session_state.palettes_list.index(palette)
     # Init image.
-    img = Image.new('RGB', (width_box, height_box))  # Create the canvas.
+    img = Image.new('RGB', (int(width_box), int(height_box)))  # Create the canvas.
     draw = ImageDraw.Draw(img)
-    col, row = grid_w, grid_h
-    colstep = int(width_box // col)
-    rowstep = int(height_box // row)
-    for r in range(row):
-        for c in range(col):
+    colstep = int(width_box // grid_w)
+    rowstep = int(height_box // grid_h)
+    for r in range(grid_h):
+        for c in range(grid_w):
             x0 = colstep * c
             y0 = rowstep * r
             x1 = colstep * (c + 1)
@@ -165,14 +98,13 @@ def generate_img(*args):
 st.title("Wallpaper generator")
 col1, col2 = st.columns(2)
 with col1:
-    st.button("Generate", key=None, help=None, on_click=change_args, args=(None, None,))
+    st.button("Generate", on_click=change_args, args=(None, None,))
 
-# Color palette Picker.
-color_expander = st.sidebar.expander("Colors Palettes", expanded=False)
-with color_expander:
+@st.cache(show_spinner=False)
+def get_palettesMiniatures():
     palette_miniatures = []
     for palette in st.session_state.palettes_list:
-        # Make a miniature pic for palette color 200x20px!.
+        # Make a miniature pic for each color color palette 200x20px.
         pal_img = Image.new('RGB', (200, 20))
         pal_draw = ImageDraw.Draw(pal_img)
         pie = 200 // len(palette.values())
@@ -181,24 +113,30 @@ with color_expander:
             x1 = pie * (step + 1)
             pal_draw.rectangle([x0, 0, x1, 20], fill=color)
         palette_miniatures.append(pal_img)
+    return palette_miniatures
 
+# Color palette Picker.
+color_expander = st.sidebar.expander("Colors Palettes", expanded=False)
+with color_expander:
     # Column for radio buttons and column for palettes pictures.
     radio_col, miniature_col = st.columns(2)
+    palette_miniatures = get_palettesMiniatures()
     with st.form(key='my_form'):
         with radio_col:
-            radio = st.radio("Pick a palette", PALETTES_NAMES, index=0, on_change=None)
+            radio = st.radio("Pick a palette", PALETTES_NAMES, index=0)
         with miniature_col:
             st.header("")
-            for i in palette_miniatures:
-                st.image(i)
+            for miniature in palette_miniatures:
+                st.image(miniature)
         submit_button = st.form_submit_button(label='Apply', on_click=change_args, args=(st.session_state.seed, radio,))
 
-def get_image_download_link(img, filename, text):
-    # Tranlate image to byt str dot download.
+@st.cache(show_spinner=False)
+def get_image_download_link(img, filename):
+    # Tranlate image to bytes str dot download.
     buffered = BytesIO()
     img.save(buffered, format="PNG")
     img_str = base64.b64encode(buffered.getvalue()).decode()
-    href =  f'<a href="data:file/txt;base64,{img_str}" style="padding:7px; border: 0.1px solid gray; font-weight: 100; color: silver; text-decoration: none; border-radius: 7%;" download="{filename}">Download</a>'
+    href =  f'<a href="data:file/txt;base64,{img_str}" style="padding: 8px;border: 0.1px solid gray;font-weight: 100;color: #FAFAFA;text-decoration: none;border-radius: 7%;" download="{filename}">Download</a>'
     return href
 
 # layout.
@@ -206,15 +144,16 @@ image_container = st.container()
 spinner_container = st.empty()
 
 # Spinner to load image.
-with col2: # On side of Generate Button.
+with col2:  # On side of Generate Button.
     with spinner_container:
         spinner = st.spinner("Generating wallpaper...")
     with spinner:
         generate_img(st.session_state.args)
         PIL_image = st.session_state.image	
         with image_container:
-            st.image(PIL_image, channels="RGBA", output_format="png")
+            st.image(PIL_image, channels="RGB", output_format="png")
 
 # Download link under image.
 with image_container:
-    st.markdown(get_image_download_link(PIL_image, "coblur.png", f"Download {width_box}x{height_box} version"), unsafe_allow_html=True)
+    name = f"wpp_{st.session_state.seed}.png"
+    st.markdown(get_image_download_link(PIL_image, name), unsafe_allow_html=True)
